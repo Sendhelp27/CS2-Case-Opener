ac3d9c5c071943ee59fabb0c30bfa53a6f7702a2
furbag75 <elliotfergo@gmail.com>
2025-01-30 20:32:51 +0800

    Update engine.py

--- engine.py
+++ engine.py
@@ -2,23 +2,29 @@
 import random as rand
 import RevolverCase as RC
 
-x = 0
+float = 0.0
 
 def get_skin_case():
     number = (rand.randint(1, 10000)) / 100
     if number <= 0.26:
         rarity = "Gold"
+        float = rand.randint(1, 20000000) / 100000000
     elif number <= 0.64:
         rarity = "Red"
+        float = rand.randint(1, 30000000) / 100000000
     elif number <= 3.2:
         rarity = "Pink"
+        float = rand.randint(1, 45000000) / 100000000
     elif number <= 15.98:
         rarity = "Purple"
+        float = rand.randint(1, 65000000) / 100000000
     else:
         rarity = "Dark Blue"
+        float = rand.randint(1, 90000000) / 100000000
     
-    return rand.choice(RC.skins_list[rarity])
+    return rand.choice(RC.skins_list[rarity]), float
 
-while x < 10:
-    print(get_skin_case())
-    x += 1
+
+print(get_skin_case())
+
+ 
