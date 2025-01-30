2f1e209ae3460fd812b61727c7fe205254431f60
Sendhelp27 <145199013+Sendhelp27@users.noreply.github.com>
2025-01-30 20:28:02 +0800

    added basic gui

--- GUI.py
+++ GUI.py
@@ -2,3 +2,19 @@ import pygame
 import engine as eng
 import RevolverCase as RC
 
+pygame.init()
+
+screen = pygame.display.set_mode([1600, 500])
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.image.load
+
+    pygame.display.flip()
+
+pygame.quit()
