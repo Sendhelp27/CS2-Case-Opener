157b35d0c05205f969b17a730b4765a718315e41
Sendhelp27 <145199013+Sendhelp27@users.noreply.github.com>
2025-01-30 19:57:23 +0800

    moved skins to case

--- engine.py
+++ engine.py
@@ -1,15 +1,9 @@
 #Revolver Case Skins
 import random as rand
+import RevolverCase as RC
 
 x = 0
 
-skins_list = {
-    "Gold": ["Bayonet|", "Flip Knife|", "Gut Knife|", "Karambit|", "M9 Bayonet|"],
-    "Red": ["M4A4 | Royal Paladin", "R8 Revolver | Fade"],
-    "Pink": ["AK-47 | Point Disarray", "G3SG1 | The Executioner", "P90 | Shapewood"],
-    "Purple": ["PP-Bizon| Fuel Rod", "Five-SeveN | Retrobution", "Negev | Power Loader", "SG 553 | Tiger Moth", "Tec-9 | Avalanche", "XM1014 | Teclu Burner"],
-    "Dark Blue": ["R8|Revolver", "AUG| Richochet", "Desert Eagle| Corinthian", "P2000| Imperial", "Sawed-Off| Yorick", "SCAR-20| Outbreak"]
-}
 
 def get_skin_revolver_case():
     number = (rand.randint(1, 10000)) / 100
@@ -24,7 +18,7 @@ def get_skin_revolver_case():
     else:
         rarity = "Dark Blue"
     
-    return rand.choice(skins_list[rarity])
+    return rand.choice(RC.skins_list[rarity])
 
 
 while x < 10:
